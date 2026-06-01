71e237bfbe35b14f7f55e50560d4a087501d5fca
mhe1239 <107838341+mhe1239@users.noreply.github.com>
2026-06-01 17:16:01 +0900

    feat: Dropout의 연산 형식 변경

--- common/layers.py
+++ common/layers.py
@@ -1,6 +1,10 @@
 """
 BatchNormalization 수정: 기존 common.layers의 BN은 Affine용(2dim)이기에 이미지의 H,W를 구분하지 못해 채널 수와 전체 크기를 맞추지 못함
-
+BatchNormalization 수정: 새로 CNN용으로 만들었으나 numerical_gradient은 노이즈가 생기듯이 BatchNormalization도 오차가 계속 발생하는 구조라서 사용하지 않기로함
+-원인: NumPy의 float64 환경에서도 np.sqrt(var + 10e-7)은 매우 미세한 노이즈로써 문제가 생김
+문제가 해결됨!!!!!!!!!!
+Dropout의 형식을 pytorch에서 쓰는 방식처럼 바꿈 '학습과 테스트 사이의 신호 세기(기댓값)를 맞춘다'라는 수학적 로직은 같지만 기존 방식은 테스트때마다 수백만 개의 뉴런에 스케일링 값을 곱해야 하는 '추론 시 오버헤드'를 가졌다면 Inverted Dropout은 학습 시에 미리 1/(1-p)를 곱해 에너지를 증폭시켜 둠으로써 테스트 단계를 '연산량 0(Identity)'의 상태로 만든다
+연산 비용을 완전히 제거한 이 방식은 대규모 시스템에서 압도적인 자원 절약 효과를 가져온다
 """
 # coding: utf-8
 import numpy as np
@@ -95,11 +99,23 @@ class SoftmaxWithLoss:
         
         return dx
 
-
+#test시에 더 빠른 속도로 연산
 class Dropout:
-    """
-    http://arxiv.org/abs/1207.0580
-    """
+    def __init__(self, dropout_ratio=0.5):
+        self.dropout_ratio = dropout_ratio
+        self.mask = None
+
+    def forward(self, x, train_flg=True):
+        if train_flg:
+            self.mask = np.random.rand(*x.shape) > self.dropout_ratio
+            # [Inverted] 학습 때 미리 비율로 나누어 에너지를 키워둠
+            return x * self.mask / (1.0 - self.dropout_ratio)
+        else:
+            # [Inverted] 테스트 때는 아무 연산 없이 통과! (속도 향상)
+            return x
+"""
+class Dropout:
+    #http://arxiv.org/abs/1207.0580
     def __init__(self, dropout_ratio=0.5):
         self.dropout_ratio = dropout_ratio
         self.mask = None
@@ -113,6 +129,8 @@ class Dropout:
 
     def backward(self, dout):
         return dout * self.mask
+"""
+
 
 """
 class BatchNormalization:
@@ -197,6 +215,7 @@ class BatchNormalization:
         return dx
 """
 #기존 common.layers의 BN은 Affine용(2dim)이기에 이미지의 H,W를 구분하지 못해 채널 수와 전체 크기를 맞추지 못함
+#->노이즈로 인해 사용하지않기로함
 class BatchNormalization:
     def __init__(self, gamma, beta, momentum=0.9, running_mean=None, running_var=None):
         self.gamma = gamma
@@ -233,7 +252,7 @@ class BatchNormalization:
             mu = x.mean(axis=0)
             xc = x - mu
             var = np.mean(xc**2, axis=0)
-            std = np.sqrt(var + 10e-7)
+            std = np.sqrt(var + 10e-5)
             xn = xc / std
             
             self.batch_size = x.shape[0]
@@ -302,6 +321,8 @@ class Convolution:
         col = im2col(x, FH, FW, self.stride, self.pad)
         col_W = self.W.reshape(FN, -1).T
         out = np.dot(col, col_W) + self.b
+        # out은 (N × out_h × out_w, FN)구조의 2차원 메트릭스 이를 다음 layer로 넘기기 위해 4차원으로 변환해야함
+        # (0:N, 1:H, 2:W, 3:C)을 (0, 3, 1, 2)로 뒤바꿔 (N, FN, out_h, out_w) format으로 만듦
         out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
         self.x = x
         self.col = col
@@ -309,9 +330,12 @@ class Convolution:
         return out
     def backward(self, dout):
         FN, C, FH, FW = self.W.shape
+        # 뒤에서 들어온 4차원을 가중치 미분(dW) 계산을 위해 다시 2차원으로 바꿈
+        # (0:N, 1:FN, 2:out_h, 3:out_w) 구조를 (N*out_h*out_w, FN)구조의 2차원 형렬로 바꿔 self.col.T와 행렬 곱이 가능하게 함
         dout = dout.transpose(0,2,3,1).reshape(-1, FN)
         self.db = np.sum(dout, axis=0)
         self.dW = np.dot(self.col.T, dout)
+        # 2차원 행렬로 구한 dW를 다시 4차원으로 복원, 모델의 실제 가중치 self.W는 (FN, C, FH, FW)의 4차원 구조이기에 (C*FH*FW, FN)구조를 다시 4차원으로 되돌림
         self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
         dcol = np.dot(dout, self.col_W.T)
         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
@@ -333,6 +357,7 @@ class Pooling:
         col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)
         col = col.reshape(-1, self.pool_h*self.pool_w)
         arg_max = np.argmax(col, axis=1)
+        # 각 윈도우에서 대푯값 하나 뽑음 
         out = np.max(col, axis=1)
         out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
         self.x = x
@@ -341,6 +366,8 @@ class Pooling:
     def backward(self, dout):
         dout = dout.transpose(0, 2, 3, 1)
         pool_size = self.pool_h * self.pool_w
+        # np.argmax로 윈도우에서 Max값(self.arg_max)을 제외하고 나머지 미분값은 0으로 채워 노이즈 없앰
+        # 역전파 시 가중치 업데이트가 핵심 특징을 유발한 뉴런들에게만 집중되어 네트워크 전반에 희소성(Sparsity)이 확보
         dmax = np.zeros((dout.size, pool_size))
         dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
         dmax = dmax.reshape(dout.shape + (pool_size,))
