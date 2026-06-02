581ffbb6d49287ce698e1da9ebc369805c88154e
mhe1239 <107838341+mhe1239@users.noreply.github.com>
2026-06-02 20:27:54 +0900

    chore: 실행 관련 수정

--- model/bg_run.ps1
+++ model/bg_run.ps1
@@ -1,2 +1,2 @@
-﻿cd "C:\pytorch_src\model\"
-python -u flexconvnet_pt.py *>&1 | Out-File -FilePath "C:\Scratch-CNN-Fashion\model\training.log" -Encoding utf8 -Append
+﻿cd "C:\Scratch-CNN-Fashion\model"
+python -u flexconvnet_pt_id6.py *>&1 | Out-File -FilePath "C:\Scratch-CNN-Fashion\model\training.log" -Encoding utf8 -Append
